bdb39d8ee57401edb5e36ed27020810a5619013f
Veronika Včelková <vcelkovav@gmail.com>
2018-10-21 19:11:55 +0200

    softmax, validation

--- main.py
+++ main.py
@@ -15,20 +15,33 @@ values[:,0] /= np.std(values[:,0])
 values[:,1] -= np.mean(values[:,1])
 values[:,1] /= np.std(values[:,1])
 
+ind = np.arange(0,len(values[:,0]))
+random.shuffle(ind)
+split =  int(len(values[:,0])* 0.8)
+train_ind = ind[:split]
+test_ind  =  ind[split:]
+
+train_inputs = values[train_ind,:]
+train_labels = labels[train_ind]
+
+test_inputs =  values[test_ind,:]
+test_labels =  labels[test_ind]
+
 #TODO posledna vrstva vzdy o velkosti poctu kategorii, dorobit zautomatizvane
 layers = [20,10,3]
+
+# sigmoid/softmax
+functions = ["sigmoid","sigmoid","sigmoid"]
 (dim, count) = values.shape
 
-#TODO shuffluj data
 secDim = count
 weights = list()
 for j in range(len(layers)):
     weights.append(np.random.normal(0, 1, [layers[j], secDim + 1]))
     secDim = layers[j]
 
-model = Model(weights)
-model.train(layers,values,labels,0.1,100)
-
-
-
+model = Model(weights,layers,functions)
+model.train(train_inputs,train_labels,0.1,20)
+CE, RE = model.test(test_inputs,test_labels)
+print('Final testing error: CE = {:6.2%}, RE = {:.5f}'.format(CE, RE))
 
--- model.py
+++ model.py
@@ -3,8 +3,10 @@ import numpy as np
 from util import *
 
 class Model():
-    def __init__(self,weights):
+    def __init__(self,weights,layers,functions):
         self.weights = weights
+        self.layers = layers
+        self.functions = functions
 
     #sigmoid
     def f_sig(self, x):
@@ -24,11 +26,47 @@ class Model():
     def df_softmax(self, x):
         y = np.e ** (x) / np.sum(np.e ** (x))
         dsoftmax = y * (1 - y)
+        return dsoftmax
 
     def cost(self,targets,outputs):
         return np.sum((targets - outputs) ** 2, axis=0)
 
-    def train(self,layers, values, labels, alpha, eps):
+    def forward(self,y,nets,activations):
+        for j in range(len(self.layers)):
+            y = np.append(y, [1], 0)
+            a = np.matmul(self.weights[j], y)
+            nets.append(a)
+            if(self.functions[j] == "sigmoid"):
+                y = self.f_sig(a)
+            if (self.functions[j] == "softmax"):
+                y = self.f_softmax(a)
+            activations.append(y)
+
+        return y, nets, activations
+
+    def predict(self, input):
+        y,*_ = self.forward(input, list(),list())
+        return y
+
+    def test(self, values, labels):
+        targets = onehot_encode(labels, 3)
+        (dim, count) = values.shape
+        CE = 0
+        RE = 0
+
+        for i in np.random.permutation(dim):
+            x = values[i]
+            d = targets[..., i]
+            y = self.predict(x)
+            CE += labels[i] != onehot_decode(y)
+            RE += self.cost(d, y)
+
+        CE /= dim
+        RE /= dim
+        return CE, RE
+
+
+    def train(self, values, labels, alpha, eps):
         (dim, count) = values.shape
         #TODO zmenit 3ku
         targets = onehot_encode(labels,3)
@@ -39,36 +77,39 @@ class Model():
             for i in np.random.permutation(dim):
                 nets = list()  #vysledok vstupov * matica vah, pred aktiv fciou
                 activations = list()
-                gradients = [None] * len(layers)
-                dW = [None] * len(layers)
+                gradients = [None] * len(self.layers)
+                dW = [None] * len(self.layers)
                 y = values[i]
                 d = targets[...,i]
 
-                for j in range(len(layers)):
-                    y = np.append(y, [1], 0)
-                    a = np.matmul(self.weights[j], y)
-                    nets.append(a)
-                    y = self.f_sig(a)
-                    activations.append(y)
+                y,nets,activations = self.forward(y,nets,activations)
 
                 CE += labels[i] != onehot_decode(y)
                 RE += self.cost(d, y)
 
                 #backpropagation
-                for j in reversed(range(len(layers))):
-                    if(j==(len(layers)-1)):
-                        gradients[j] = ((d - y) * self.df_sig(nets[j]))
+                for j in reversed(range(len(self.layers))):
+                    if(j==(len(self.layers)-1)):
+                        if(self.functions[j] == "sigmoid"):
+                            gradients[j] = ((d - y) * self.df_sig(nets[j]))
+                        if (self.functions[j] == "softmax"):
+                            gradients[j] = ((d - y) * self.df_softmax(nets[j]))
                         dW[j] = np.outer(gradients[j], np.append(activations[j - 1],[1],0))
                     else:
-                        gradients[j] = np.matmul(np.transpose(self.weights[j + 1][:, :-1]), gradients[j + 1]) * self.df_sig(nets[j])
+                        if (self.functions[j] == "sigmoid"):
+                            gradients[j] = np.matmul(np.transpose(self.weights[j + 1][:, :-1]),
+                                                     gradients[j + 1]) * self.df_sig(nets[j])
+                        if (self.functions[j] == "softmax"):
+                            gradients[j] = np.matmul(np.transpose(self.weights[j + 1][:, :-1]),
+                                                     gradients[j + 1]) * self.df_softmax(nets[j])
                         if(j==0):
                             dW[j] = np.outer(gradients[j], np.append(values[i], [1], 0))
                         else:
                             dW[j] = np.outer(gradients[j], np.append(activations[j - 1], [1], 0))
 
-                for j in range(len(layers)):
+                for j in range(len(self.layers)):
                     self.weights[j] += alpha * dW[j]
 
             CE /= dim
             RE /= dim
-            print('CE = {:6.2%}, RE = {:.5f}'.format(CE, RE))
+            print(ep+1,'epoch, CE = {:6.2%}, RE = {:.5f}'.format(CE, RE))
